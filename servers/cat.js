c9a67a450efbe8d239330665de8d01d37d3edd2d
pearpages <pere@soms.cat>
2016-03-03 18:16:08 +0100

    running multiple servers at the same time

--- servers/cat.js
+++ servers/cat.js
@@ -10,7 +10,7 @@ mongoose.connect('mongodb://localhost/cats');
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-var cats = require('./cat_routes.js')(app);
+var catRoutes = require('../routes/cat.js')(app);
 
 var server = app.listen(3000, function() {
     console.log('Server running at http://127.0.0.1:3000/');
