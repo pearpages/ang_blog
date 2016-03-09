44a137ae2676bdff756b120c9de724b9a80529e2
pearpages <pere@soms.cat>
2016-03-09 15:57:33 +0100

    testing models

---
+++ non-persistent-models/Student.js
@@ -0,0 +1,27 @@
+'use strict';
+
+var uuid = require("uuid"); // for generating ids
+
+function Student() {}
+
+Student.create = function (name, grade) {
+	var student = new Student();
+
+	student.name = name;
+	student.grade = grade;
+	student.id = uuid.v4();	
+
+	return student;
+};
+
+var _p = Student.prototype;
+
+_p.advanceGrade = function () {
+	this.grade++;
+};
+
+_p.toString = function () {
+	return this.id + "\t" + this.name;
+};
+
+module.exports = Student;
